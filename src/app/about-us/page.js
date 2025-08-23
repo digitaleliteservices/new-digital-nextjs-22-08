8a907b10cf645c507bc51c6767c31e57ca50f2a5
RajavrukshaGroups <technical@rajavrukshagroup.in>
2025-08-23 14:41:28 +0530

    many changes

---
+++ src/app/about-us/page.js
@@ -0,0 +1,17 @@
+"use client";
+
+import AboutUsComp from "../_components/aboutUsComp/page";
+import Footer from "../footer/page";
+import Navbar from "../navbar/page";
+
+const AboutUs = () => {
+  return (
+    <div>
+      <Navbar />
+      <AboutUsComp />
+      <Footer />
+    </div>
+  );
+};
+
+export default AboutUs;
